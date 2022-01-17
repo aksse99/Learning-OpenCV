3bc307ad558b0f83aebe90cf00683d9a26d07472
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-17 11:25:25 +0530

    Create color_using_webcam.py

---
+++ color_using_webcam.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+frameWidth = 200
+frameHeight = 300
+vid = cv2.VideoCapture(0)
+vid.set(3,frameWidth)
+vid.set(4,frameHeight)
+vid.set(10,200)
+
+
+
+
+
+def empty(a):
+    pass
+
+
+
+cv2.namedWindow("Trackbars")
+cv2.resizeWindow("Trackbars", (500,240))
+cv2.createTrackbar("Hue Min", "Trackbars",0,179,empty)
+cv2.createTrackbar("Hue Max", "Trackbars",97,179,empty)
+cv2.createTrackbar("Sat Min", "Trackbars",111,255,empty)
+cv2.createTrackbar("Sat Max", "Trackbars",255,255,empty)
+cv2.createTrackbar("Val Min", "Trackbars",30,255,empty)
+cv2.createTrackbar("Val Max", "Trackbars",255,255,empty)
+
+
+while True:
+    _, img = vid.read()
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "Trackbars")
+    h_max = cv2.getTrackbarPos("Hue Max", "Trackbars")
+    s_min = cv2.getTrackbarPos("Sat Min", "Trackbars")
+    s_max = cv2.getTrackbarPos("Sat Max", "Trackbars")
+    v_min = cv2.getTrackbarPos("Val Min", "Trackbars")
+    v_max = cv2.getTrackbarPos("Val Max", "Trackbars")
+    print(h_min,h_max,s_min,s_max,v_min,v_max)
+    lower = np.array([h_min,s_min,v_min])
+    upper = np.array([h_max,s_max,v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img,img,mask=mask)
+
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+
+    cv2.imshow("Original", img)
+    cv2.imshow("HSV", imgHSV)
+    cv2.imshow("Mask", mask)
+    cv2.imshow("Result", imgResult)
+    if cv2.waitKey(1) & 0xff ==ord('q'):
+        break
+
+
